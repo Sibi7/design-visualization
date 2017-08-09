c16fbe5afbee090412c1b2ed31ccd10201b457a1
AKD47 <van83horn@gmail.com>
2017-08-09 15:31:35 +0300

    scroll script

--- app/js/script.js
+++ app/js/script.js
@@ -9,6 +9,14 @@ $(document).ready(function () {
         $('html,body').animate({scrollTop: top}, 1000);
         return false;
     });
+    $(document).on('click', '.footer__menu a', function (event) {
+        event.preventDefault();
+        var href = $(this).attr('href');
+        var target = $(href);
+        var top = target.offset().top;
+        $('html,body').animate({scrollTop: top}, 1000);
+        return false;
+    });
    /*close*/
 
     //flexslider for consultation
